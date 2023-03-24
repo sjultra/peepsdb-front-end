ee66cf9abdd5e83bf3818ede3b02adb1368a91d8
Andrei <andreinicolaepaun@gmail.com>
2023-03-24 11:32:29 +0000

    index.js and app.js have been changed to use createRoot feature of React 18; removed store.js file  from root (not redux store folder)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 // import Alert from './components/layouts/Alert';
 import NotFoundScreen from './screens/NotFoundScreen';
 import DashboardScreen from './screens/DashboardScreen';
@@ -23,6 +24,7 @@ import WorkerWorkspacesScreen from './screens/WorkerWorkspacesScreen';
 import ModalComponent from './components/layouts/Modal';
 import Audit from './screens/Admin/audit';
 import useAppInsights from './hooks/useAppInsights';
+import { MenuProvider } from './hooks/MenuProvider';
 import User from './screens/Admin/user';
 
 const App = () => {
@@ -35,36 +37,44 @@ const App = () => {
   useEffect(() => initAzureLoggingRef.current(), []);
 
   return (
-    <Router>
-      {/* <Alert /> */}
+    <MenuProvider>
+      <ChakraProvider>
+        <Router>
+          {/* <Alert /> */}
+          <ModalComponent />
+          <Switch>
+            <Route path="/login" component={LoginScreen} />
+            <PrivateRoute exact path="/" component={DashboardScreen} />
+            <PrivateRoute path="/profile" component={ProfileScreen} />
+            <PrivateRoute path="/edit-profile" component={EditProfileScreen} />
+            <PrivateRoute
+              path="/ado_workitems/:id"
+              component={ADOWorkItemsScreen}
+            />
+            <PrivateRoute
+              path="/jira_issues/:id"
+              component={JiraIssuesScreen}
+            />
+            <PrivateRoute path="/meeting" component={MeetingScheduleScreen} />
+            <PrivateRoute
+              path="/worker/workspaces"
+              component={WorkerWorkspacesScreen}
+            />
+            <AdminRoute path="/admin/logs" component={Audit} />
+            <AdminRoute path="/admin/users/" component={AdminUsers} />
+            <AdminRoute path="/admin/workspaces" component={AdminWorkspace} />
+            <AdminRoute path="/admin/user/:id" component={User} exact />
 
-      <ModalComponent />
-
-      <Switch>
-        <Route path="/login" component={LoginScreen} />
-        <PrivateRoute exact path="/" component={DashboardScreen} />
-        <PrivateRoute path="/profile" component={ProfileScreen} />
-        <PrivateRoute path="/edit-profile" component={EditProfileScreen} />
-        <PrivateRoute
-          path="/ado_workitems/:id"
-          component={ADOWorkItemsScreen}
-        />
-        <PrivateRoute path="/jira_issues/:id" component={JiraIssuesScreen} />
-        <PrivateRoute path="/meeting" component={MeetingScheduleScreen} />
-        <PrivateRoute
-          path="/worker/workspaces"
-          component={WorkerWorkspacesScreen}
-        />
-        <AdminRoute path="/admin/logs" component={Audit} />
-        <AdminRoute path="/admin/users/" component={AdminUsers} />
-        <AdminRoute path="/admin/workspaces" component={AdminWorkspace} />
-        <AdminRoute path="/admin/user/:id" component={User} exact />
-
-        {/* <PrivateRoute path='/admin/users/:id' component={UserScreen} exact /> */}
-        <PrivateRoute path="/admin/users/:id/edit" component={UserEditScreen} />
-        <Route component={NotFoundScreen} />
-      </Switch>
-    </Router>
+            {/* <PrivateRoute path='/admin/users/:id' component={UserScreen} exact /> */}
+            <PrivateRoute
+              path="/admin/users/:id/edit"
+              component={UserEditScreen}
+            />
+            <Route component={NotFoundScreen} />
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </MenuProvider>
   );
 };
 
