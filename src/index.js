f04ac1f78617faf39a5cea1b54140617823e05f2
Dayvvo <dyvvoo@gmail.com>
2023-03-31 21:33:57 +0100

    revert to react.createToot

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 //follow come
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // self imported
 import { Provider } from 'react-redux';
 import store from './store/index';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  ,
-  document.getElementById("root")
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
