975db2073e053706abde9fe14cf58f49178e5b0d
Oreol Noumodong <oreolnoumodong@gmail.com>
2023-06-20 19:19:21 +0100

    refactor

--- __checks__/login.spec.js
+++ __checks__/login.spec.js
@@ -1,21 +1,7 @@
 const { chromium } = require("playwright")
 const { test, expect } = require("@playwright/test")
 
-test("Make sure login with google account is working well", async () => {
-  // Launch the Chromium browser
-  const browser = await chromium.launch()
-
-  // Create a new browser context with a user agent string
-  const context = await browser.newContext({
-    userAgent:
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-  })
-
-  // Create a new page in the browser context and navigate to target URL
-  const page = await context.newPage()
-
-  // Resize window to 1280 x 721
-  await page.setViewportSize({ width: 1280, height: 721 })
+test("Make sure login with google account is working well", async ({page}) => {
 
   // Load "https://peepsdb.vzxy.net/login"
   await page.goto("https://peepsdb.vzxy.net/login")
@@ -41,7 +27,7 @@ test("Make sure login with google account is working well", async () => {
 
   await page.screenshot({ path: "homepage.jpg" })
 
-  await browser.close();
+  await page.close();
 
 })
   
